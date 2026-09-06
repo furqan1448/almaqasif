3a4a1a028f4167300c0196811426613e16d8808d
furqan1448 <maram1998m3@gmail.com>
2026-09-06 18:07:09 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // ⚠️ حطي هنا رابط الـ Web app اللي طلعلك من Google Apps Script بعد الـ Deploy
-const API_URL = "https://script.google.com/macros/s/AKfycbzCAwixPjT7PvFAaQw8r9riJNxndNCR1pHKpNgb1Ha21kxU-2r24pinRpbUVrogJm0d/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbwU-ASURUawgP0y7MLcgccVTtIQk69jE4vUduDIxjQLz_bEI9WioJqrmmo6hlPd2zCo/exec";
 
 async function callApi(action, data) {
   const payload = Object.assign({ action: action }, data || {});
