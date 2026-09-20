3a54fe5945c7d848c88f8e94d2d9362a508da60b
furqan1448 <maram1998m3@gmail.com>
2026-09-20 16:52:08 +0300

    Update API_URL to new Google Apps Script link

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // ⚠️ حطي هنا رابط الـ Web app اللي طلعلك من Google Apps Script بعد الـ Deploy
-const API_URL = "https://script.google.com/macros/s/AKfycbyBr5KMm5T4sxL6fHOcQ2fK5Ecl0HH_X4NRuZd0xmIMdZygYQAyDc6_PvnhTdNqobbw/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzIr0dZxkSp0gjGGjoB402PGFJI5fKkFhLubWX4HK6ohTgnwJ05thzoqMVQeAqu-O2i/exec";
 
 /* ------------------- تخزين مؤقت خفيف من جهة المتصفح لطلبات القراءة -------------------
    الهدف: تقليل عدد الطلبات لـ Apps Script بدون تغيير أي نتيجة أو سلوك ظاهر للمستخدمة.
