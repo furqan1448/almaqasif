654e41b6ae12b0b617096beeb9c6f0244be417ab
furqan1448 <maram1998m3@gmail.com>
2026-09-23 08:38:32 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // ⚠️ حطي هنا رابط الـ Web app اللي طلعلك من Google Apps Script بعد الـ Deploy
-const API_URL = "https://script.google.com/macros/s/AKfycbxwZPvaQECqotOfjzMP_aW01mmwpX4MKk5r-va1eRvFOax-ZdTH1EFCHn68yIYNgtkf/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbwLBzFwcefPbhBmguR-WtiJ93B2NZCIgZhruzF4uPFg_5PRK9pPuBHa6K7hH9SI4JmF/exec";
 
 /* ------------------- تخزين مؤقت خفيف من جهة المتصفح لطلبات القراءة -------------------
    الهدف: تقليل عدد الطلبات لـ Apps Script بدون تغيير أي نتيجة أو سلوك ظاهر للمستخدمة.
